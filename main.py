16d7c359f1548f2b6db2fcf742a287fcb1d2b640
d.bogach <91407532+Danil1303@users.noreply.github.com.>
2025-02-27 09:59:32 +0300

    Файлы для Docker

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import time
 import requests
 import threading
@@ -8,7 +7,6 @@ from flask import Flask, request
 from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
-data = []
 
 
 def get_data_from_delta() -> None:
@@ -26,21 +24,15 @@ def get_data_from_delta() -> None:
         if response.status_code == 200:
             data = [i['grz'] for i in response.json()]
         else:
-            send_message(f'Произошла ошибка: {response.status_code}')
+            print(f'Произошла ошибка: {response.status_code}')
     except Exception as e:
-        send_message(f'Произошла ошибка: {e}')
+        print(f'Произошла ошибка: {e}')
 
 
 def periodic_data_update() -> None:
     while True:
         get_data_from_delta()
         time.sleep(300)
-
-
-def send_message(message: str) -> None:
-    credentials = get_credentials({'TOKEN': '', 'chat_id': ''})
-    url = f'https://api.telegram.org/bot{credentials['TOKEN']}/sendMessage?chat_id={credentials['chat_id']}&text={message}'
-    requests.get(url).json()
 
 
 def get_credentials(credentials: dict[str, str]) -> dict[str, str]:
@@ -54,7 +46,7 @@ def get_credentials(credentials: dict[str, str]) -> dict[str, str]:
                         break
         return credentials
     except Exception as e:
-        send_message(f'Произошла ошибка: {e}')
+        print(f'Произошла ошибка: {e}')
 
 
 # Обработчик маршрута /get_status
@@ -62,7 +54,7 @@ def get_credentials(credentials: dict[str, str]) -> dict[str, str]:
 def get_status():
     global data
     if not data:
-        send_message(f'Произошла ошибка: данные не получены')
+        print(f'Произошла ошибка: данные не получены')
 
     # Получаем параметр 'request_number' из URL
     request_number = request.args.get('number')
@@ -88,4 +80,4 @@ if __name__ == '__main__':
     # Запускаем сервер на всех интерфейсах, на порту 5001
     serve(app, host='0.0.0.0', port=5001)
 
-# pyinstaller -w -F --onefile main.py
+# docker-compose up -d --build
